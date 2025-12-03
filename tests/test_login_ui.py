fb1380640edc39dab15612dfa7263b83fb92dd08
balaji-githubstore <balajidinakaran1@gmail.com>
2025-12-03 09:55:38 +0530

    Added test_placeholder method

--- tests/test_login_ui.py
+++ tests/test_login_ui.py
@@ -26,6 +26,7 @@ class TestLoginUI:
         assert_that("Login").is_equal_to(actual_header)
 
     def test_placeholder(self):
-        print("placeholder")
-#         Assert Username placeholder  - Username
-#           Assert Password placeholder  - Username
+        actual_username_placeholder= self.driver.find_element(By.NAME,"username").get_attribute("placeholder")
+        actual_password_placeholder= self.driver.find_element(By.NAME,"password").get_attribute("placeholder")
+        assert_that("Username").is_equal_to(actual_username_placeholder)
+        assert_that("Password").is_equal_to(actual_password_placeholder)

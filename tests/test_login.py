cb751b440dddbf55312110f0c29ee609dae8707d
balaji-githubstore <balajidinakaran1@gmail.com>
2025-12-03 12:07:54 +0530

    Added test_valid_login and test_invalid_login method - Added DataSource

--- tests/test_login.py
+++ tests/test_login.py
@@ -3,21 +3,20 @@ from assertpy import assert_that
 from selenium.webdriver.common.by import By
 
 from base.automation_wrapper import AutomationWrapper
+from utilities.data_source import DataSource
 
 
 class TestLogin(AutomationWrapper):
 
-    def test_valid_login(self):
-        self.driver.find_element(By.NAME, "username").send_keys("Admin")
-        self.driver.find_element(By.NAME, "password").send_keys("admin123")
+    @pytest.mark.parametrize("username,password,expected_value", DataSource.data_valid_login)
+    def test_valid_login(self, username, password, expected_value):
+        self.driver.find_element(By.NAME, "username").send_keys(username)
+        self.driver.find_element(By.NAME, "password").send_keys(password)
         self.driver.find_element(By.XPATH, "//button[contains(normalize-space(),'Login')]").click()
         actual_value = self.driver.find_element(By.XPATH, "//p[contains(normalize-space(),'Quick')]").text
-        assert_that("Quick Launch").is_equal_to(actual_value)
+        assert_that(expected_value).is_equal_to(actual_value)
 
-    @pytest.mark.parametrize("username, password, expected_error", [
-        ("saul", "saul123", "Invalid credentials"),
-        ("peter", "pete123", "Invalid credentials")
-    ])
+    @pytest.mark.parametrize("username, password, expected_error", DataSource.data_invalid_login)
     def test_invalid_login(self, username, password, expected_error):
         self.driver.find_element(By.NAME, "username").send_keys(username)
         self.driver.find_element(By.NAME, "password").send_keys(password)
--- utilities/data_source.py
+++ utilities/data_source.py
@@ -1,3 +1,9 @@
+""" This module contains data for all test methods """
+
+
 class DataSource:
     data_invalid_login = [("saul", "saul123", "Invalid credentials"),
                           ("peter", "pete123", "Invalid credentials")]
+
+    data_valid_login = [("Admin", "admin123", "Quick Launch"),
+                        ("Admin", "admin123", "Quick Launch")]

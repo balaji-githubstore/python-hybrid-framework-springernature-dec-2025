d4ad1e670b76a9f28424572db4670d89d4920e8f
balaji-githubstore <balajidinakaran1@gmail.com>
2025-12-03 11:51:38 +0530

    Added test_valid_login and test_invalid_login method

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,3 +1,4 @@
+import pytest
 from assertpy import assert_that
 from selenium.webdriver.common.by import By
 
@@ -13,9 +14,13 @@ class TestLogin(AutomationWrapper):
         actual_value = self.driver.find_element(By.XPATH, "//p[contains(normalize-space(),'Quick')]").text
         assert_that("Quick Launch").is_equal_to(actual_value)
 
-    def test_invalid_login(self):
-        self.driver.find_element(By.NAME, "username").send_keys("john")
-        self.driver.find_element(By.NAME, "password").send_keys("john123")
+    @pytest.mark.parametrize("username, password, expected_error", [
+        ("saul", "saul123", "Invalid credentials"),
+        ("peter", "pete123", "Invalid credentials")
+    ])
+    def test_invalid_login(self, username, password, expected_error):
+        self.driver.find_element(By.NAME, "username").send_keys(username)
+        self.driver.find_element(By.NAME, "password").send_keys(password)
         self.driver.find_element(By.XPATH, "//button[contains(normalize-space(),'Login')]").click()
         actual_value = self.driver.find_element(By.XPATH, "//p[contains(normalize-space(),'Invalid')]").text
-        assert_that("Invalid credentials").contains(actual_value)
+        assert_that(expected_error).contains(actual_value)
---
+++ utilities/data_source.py
@@ -0,0 +1,3 @@
+class DataSource:
+    data_invalid_login = [("saul", "saul123", "Invalid credentials"),
+                          ("peter", "pete123", "Invalid credentials")]
